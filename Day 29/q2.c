e12ab7242a0b36eeddb998873cd5a662d5595d9d
Mukund Rawlley <mukundrawlley@gmail.com>
2026-03-01 00:00:00 +0530

    Day 29 - 100 Days of DSA

---
+++ Day 29/q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode* current = &dummy;
+    int carry = 0;
+    
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+        
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        
+        carry = sum / 10;
+        current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+        current->next->val = sum % 10;
+        current->next->next = NULL;
+        current = current->next;
+    }
+    
+    return dummy.next;
+}
