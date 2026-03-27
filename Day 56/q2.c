42a77f838c4d1a850909fff7c4b5bc392550ef87
Mukund Rawlley <mukundrawlley@gmail.com>
2026-03-28 00:00:00 +0530

    Day 56 - 100 Days of DSA

---
+++ Day 56/q2.c
@@ -0,0 +1,18 @@
+bool isMirror(struct TreeNode* left, struct TreeNode* right) {
+    if(left == NULL && right == NULL)
+        return true;
+
+    if(left == NULL || right == NULL)
+        return false;
+
+    if(left->val != right->val)
+        return false;
+
+    return isMirror(left->left, right->right) && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if(root == NULL)
+        return true;
+    return isMirror(root->left, root->right);
+}
